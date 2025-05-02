30d0f6681437291f32bfb77285ef5e5d0522984d
Ting Wei Fan <tfan2437@gmail.com>
2025-05-02 08:12:53 +0800

    Add SSO callback route and enhance AuthCallbackPage for user synchronization

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import { Routes, Route } from "react-router-dom";
 // pages
 import HomePage from "@/pages/HomePage";
 import AuthCallbackPage from "@/pages/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signInForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        />
+
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
       </Routes>
     </>
